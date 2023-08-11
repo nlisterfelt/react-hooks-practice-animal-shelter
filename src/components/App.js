bf45fe126bf16290c12f41de7464a1f43451c108
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-10 21:16:01 -0500

    passed all tests

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,26 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
-
+  function handleFindPetsClick(){
+    if(filters.type==="all"){
+      fetch('http://localhost:3001/pets')
+      .then(r=>r.json())
+      .then(data=>setPets(data))
+    } else {
+      fetch(`http://localhost:3001/pets?type=${filters.type}`)
+      .then(r=>r.json())
+      .then(data=>setPets(data))
+    }
+  }
+  function handleAdoptPet(id){
+    const updatedPets = pets.map(pet=>{
+      if(pet.id===id){
+        pet.isAdopted=true
+      }
+      return pet
+    })
+    setPets(updatedPets)
+  }
   return (
     <div className="ui container">
       <header>
@@ -15,10 +34,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={setFilters} onFindPetsClick={handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,13 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({pets, onAdoptPet}) {
+  const petJSX = pets.map(pet=><Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet}/>)
+  return (
+  <div className="ui cards">
+    {petJSX}
+  </div>
+  )
 }
 
 export default PetBrowser;
